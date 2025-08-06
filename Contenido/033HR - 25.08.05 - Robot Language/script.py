8bbd9afea8e88278dcaef3f616e90a0df38604ba
Lersgeeb <banegas.gabriel13@gmail.com>
2025-08-05 21:24:00 -0600

    Refactor script to enhance character translation and display functionality

--- Contenido/033HR - 25.08.05 - Robot Language/script.py
+++ Contenido/033HR - 25.08.05 - Robot Language/script.py
@@ -2,19 +2,47 @@ from collections import Counter
 
 possible_password = "⧈⨂⧉╲⧉╲⦿⨂⧉⦿⧈⌂⦿⧉"
 
+# Show different characters at password
+possible_password_chars = set(possible_password)
+print("Caracteres en la posible contraseña:", possible_password_chars)
+
 list_chars_str = "█ : ◉ : ╳ : ⧈ : ⧉ : ▢ : ◌ : ⨂ : ╲ : ⌂ : ⧉ : ⌂ : ⨀ : ◀ : ⦿ : ⧈ : ⦿ : ⨂ : ⌂ : ╲ : ⦿ : ⧉ : ⨀ : ◀ : ⦿ : ⧈" 
+abc = "abcdefghijklmnopqrstuvwxyz"
 list_chars = list_chars_str.split(" : ")
 char_count = Counter(list_chars)
 list_chars_unicode = []
 
-print(list_chars)
+def find_all_char_positions(text_string, char_to_find):
+    positions = []
+    for i, char in enumerate(text_string):
+        if char == char_to_find:
+            positions.append(abc[i])
+    return positions
 
-# O mostrar ordenado por carácter
+
+
+translated_chars = []
+for char in possible_password:
+    translated_chars.append(find_all_char_positions("".join(list_chars), char))
+
+# Mostrar caracteres traducidos
+print("Caracteres traducidos:", translated_chars)
+Drviktorkozlov
+
+
+
+# Mostrar list_chars
+print("List Chars:", list_chars)
+
+# Mostrar tamaño lista
+print("Tamaño lista:", len(list_chars))
+
+# Mostrar ordenado por carácter y mostrar unicode
 print("\nOrdenado por carácter:")
 for char in sorted(char_count.keys()):
-    print(f"'{char}': {char_count[char]}")
+    print(f"'{char}' (Unicode: {char.encode('unicode_escape')}): {char_count[char]}")
 
-# O mostrar ordenado por cantidad (descendente)
+# Mostrar ordenado por cantidad (descendente)
 print("\nOrdenado por cantidad:")
 for char, count in char_count.most_common():
     print(f"'{char}': {count}")
